9620b94cfc02e5d8cea52a09882ba9836da838d5
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-04 18:12:41 +0900

    missing and shadow links classes work

--- plugin/LowerLinkWidget.js
+++ plugin/LowerLinkWidget.js
@@ -6,22 +6,29 @@ module-type: widget
 Override link widget.
 \*/
 
-(function () {
+(function() {
 
     /*jslint node: true, browser: true */
     /*global $tw: false */
     "use strict";
 
     var LinkWidget = require("$:/core/modules/widgets/link.js").link;
+
     function LowerLinkWidget(parseTreeNode, options) {
         LinkWidget.call(this, parseTreeNode, options);
     };
 
     LowerLinkWidget.prototype = Object.create(LinkWidget.prototype);
 
-    LowerLinkWidget.prototype.execute = function () {
-        var returnVal = LinkWidget.prototype.execute.call(this)
-        this.to = this.wiki.getIndexer("LowerTitleIndexer").lookup(this.to)
+    LowerLinkWidget.prototype.execute = function() {
+        var returnVal = LinkWidget.prototype.execute.call(this);
+        var realTo = this.wiki.getIndexer("LowerTitleIndexer").lookup(this.to);
+        if (realTo) {
+            this.to = realTo
+            this.isMissing = !this.wiki.tiddlerExists(this.to);
+            this.isShadow = this.wiki.isShadowTiddler(this.to);
+        }
+
         return returnVal
     }
 
